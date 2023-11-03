2f2a05cdc906833ea61a87be10221cbb1d6e883b
Jesus Machacon Pacheco <machacon74@gmail.com>
2023-11-03 01:12:22 -0500

    Se crea repositorio Estudiante

---
+++ UniversidadApi/Repositories/EstudianteRepository/EstudianteRepository.cs
@@ -0,0 +1,12 @@
+﻿using UniversidadApi.Models;
+using UniversidadApi.Repositories.GenericRepository;
+
+namespace UniversidadApi.Repositories.EstudianteRepository
+{
+    public class EstudianteRepository : GenericRepository<Estudiante, int>, IEstudianteRepository
+    {
+        public EstudianteRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
