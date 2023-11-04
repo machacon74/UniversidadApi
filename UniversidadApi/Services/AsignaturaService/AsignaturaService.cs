8f68c1e2dd3cdbaa90235e4be978e222747554d7
Jesus Machacon Pacheco <machacon74@gmail.com>
2023-11-03 22:32:47 -0500

    Se crea el service para asignaturas

--- UniversidadApi/Services/AsignaturaService/AsignaturaService.cs
+++ UniversidadApi/Services/AsignaturaService/AsignaturaService.cs
@@ -13,24 +13,24 @@ namespace UniversidadApi.Services.AsignaturaService
 
         public async Task<List<Asignatura>> GetAll()
         {
-            return await UnitOfWork.asignaturas.GetAll().ToListAsync();
+            return await UnitOfWork.AsignaturaRepository.GetAll().ToListAsync();
         }
 
-        public async Task<Genero?> GetById(short id)
+        public async Task<Asignatura?> GetById(int id)
         {
-            return await UnitOfWork.GeneroRepository.GetByID(id);
+            return await UnitOfWork.AsignaturaRepository.GetByID(id);
         }
 
-        public async Task<Genero?> Add(Genero entity)
+        public async Task<Asignatura?> Add(Asignatura entity)
         {
-            var genero = await UnitOfWork.GeneroRepository.Add(entity);
+            var genero = await UnitOfWork.AsignaturaRepository.Add(entity);
             await UnitOfWork.SaveChanges();
             return genero;
         }
 
-        public async Task<Genero?> Update(Genero entity)
+        public async Task<Asignatura?> Update(Asignatura entity)
         {
-            var genero = UnitOfWork.GeneroRepository.Update(entity);
+            var genero = UnitOfWork.AsignaturaRepository.Update(entity);
             await UnitOfWork.SaveChanges();
             return genero;
         }
