2a1a353e879dae3e27b359215abec85ae9ebfe23
Jesus Machacon Pacheco <machacon74@gmail.com>
2023-11-03 22:28:29 -0500

    Se agrega repository de calificaciones a Unit Of Work

--- UniversidadApi/Data/UnitOfWork.cs
+++ UniversidadApi/Data/UnitOfWork.cs
@@ -1,4 +1,5 @@
 ﻿using UniversidadApi.Repositories.AsignaturaRepository;
+using UniversidadApi.Repositories.CalificacionRepository;
 using UniversidadApi.Repositories.EstudianteRepository;
 using UniversidadApi.Repositories.GeneroRepository;
 
@@ -9,6 +10,7 @@ namespace UniversidadApi.Data
         public IGeneroRepository GeneroRepository { get; }
         public IAsignaturaRepository AsignaturaRepository { get; }
         public IEstudianteRepository EstudianteRepository { get; }
+        public ICalificacionRepository CalificacionRepository { get; }
         
         Task<int> SaveChanges();
     }
@@ -18,13 +20,15 @@ namespace UniversidadApi.Data
         public IGeneroRepository GeneroRepository { get; }
         public IAsignaturaRepository AsignaturaRepository { get; }
         public IEstudianteRepository EstudianteRepository { get; }
+        public ICalificacionRepository CalificacionRepository { get; }
 
-        public UnitOfWork(DbContext context, IGeneroRepository generoRepository, IAsignaturaRepository asignaturaRepository, IEstudianteRepository estudianteRepository)
+        public UnitOfWork(DbContext context, IGeneroRepository generoRepository, IAsignaturaRepository asignaturaRepository, IEstudianteRepository estudianteRepository, ICalificacionRepository calificacionRepository)
         {
             _context = context;
             GeneroRepository = generoRepository;
             AsignaturaRepository = asignaturaRepository;
             EstudianteRepository = estudianteRepository;
+            CalificacionRepository = calificacionRepository;
         }
 
         public void Dispose() => _context.Dispose();
