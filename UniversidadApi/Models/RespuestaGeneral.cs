c74a6ef8b9ade1dd1f68addd17850142b5040448
Jesus Machacon Pacheco <machacon74@gmail.com>
2023-11-04 17:43:31 -0500

    Se crea clase RespuestaGeneral que contendrá respuesta de validaciones

---
+++ UniversidadApi/Models/RespuestaGeneral.cs
@@ -0,0 +1,14 @@
+﻿namespace UniversidadApi.Models
+{
+    public class RespuestaGeneral
+    {
+        public int Codigo { get; set; } = 0;
+        public string Mensaje { get; set; } = string.Empty;
+
+        public RespuestaGeneral(int codigo, string mensaje = "")
+        {
+            Codigo = codigo;
+            Mensaje = mensaje;
+        }
+    }
+}
