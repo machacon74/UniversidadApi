b3c93931cce0a7292deb932b9ac4d5ee72d491df
Jesus Machacon Pacheco <machacon74@gmail.com>
2023-11-07 23:41:40 -0500

    Se modifica EstudianteService para retornar respuesta general

--- UniversidadApi/Services/EstudianteService/EstudianteService.cs
+++ UniversidadApi/Services/EstudianteService/EstudianteService.cs
@@ -11,28 +11,41 @@ namespace UniversidadApi.Services.EstudianteService
             _unitOfWork = unitOfWork;
         }
 
-        public async Task<Estudiante?> Add(Estudiante estudiante)
+        public async Task<RespuestaGeneral> Add(Estudiante estudiante)
         {
+
             estudiante = await _unitOfWork.EstudianteRepository.Add(estudiante);
             await _unitOfWork.SaveChanges();
-            return estudiante;
+            return new RespuestaGeneral(1)
+            {
+                DatosRespuesta = estudiante
+            };
         }
 
-        public async Task<List<Estudiante>> GetAll()
+        public async Task<RespuestaGeneral> GetAll()
         {
-            return await _unitOfWork.EstudianteRepository.GetAll().ToListAsync();
+            return new RespuestaGeneral(1)
+            {
+                DatosRespuesta = await _unitOfWork.EstudianteRepository.GetAll().ToListAsync()
+            };
         }
 
-        public async Task<Estudiante?> GetById(int id)
+        public async Task<RespuestaGeneral> GetById(int id)
         {
-            return await _unitOfWork.EstudianteRepository.GetByID(id);
+            return new RespuestaGeneral(1)
+            {
+                DatosRespuesta = await _unitOfWork.EstudianteRepository.GetByID(id)
+            };
         }
 
-        public async Task<Estudiante?> Update(Estudiante estudiante)
+        public async Task<RespuestaGeneral> Update(Estudiante estudiante)
         {
             estudiante = _unitOfWork.EstudianteRepository.Update(estudiante);
             await _unitOfWork.SaveChanges();
-            return estudiante;
+            return new RespuestaGeneral(1)
+            {
+                DatosRespuesta = estudiante
+            };
         }
     }
 }
