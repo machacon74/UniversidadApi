73fd2515ee1d848d3abd5ba3859cd9435fc74737
Jesus Machacon Pacheco <machacon74@gmail.com>
2023-11-08 01:52:23 -0500

    Se configuara serializacion de JSONglobal Se cambia nombre de proyecto de BD

--- UniversidadApi/Program.cs
+++ UniversidadApi/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.Configuration;
+using System.Text.Json;
 using UniversidadApi.Repositories.AsignaturaRepository;
 using UniversidadApi.Repositories.CalificacionRepository;
 using UniversidadApi.Repositories.EstudianteRepository;
@@ -12,8 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c => {
     c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
